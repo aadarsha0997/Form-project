5b82dcdf8c47ea51fce300ad5ad377268d744285
Ayush <aadarsha0997@gmail.com>
2024-11-08 14:53:20 +0545

    complete of project

--- form/urls.py
+++ form/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("",views.form)
+    path("",views.signup,name="signup"),
+    path("login/",views.user_login,name="user_login"),
+    path("home/",views.home_page,name="home"),
+    path("logout/",views.logout_page,name="logout"),
 ]
